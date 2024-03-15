73c1ddc3c95eb4a022cae2f4ea440b70d9013f56
Xinzhe.Gao <gao-yiqian@outlook.com>
2024-03-15 13:37:12 +0900

    update

--- components/Common/Logo.js
+++ components/Common/Logo.js
@@ -2,7 +2,7 @@
 import * as React from 'react'
 
 const Logo = (props) => (
-  <img src="https://cdn.jsdelivr.net/gh/An-Jhon/image_bed/202402231446700.png" width="30" height="30" />
+  <img src="https://ice.frostsky.com/2024/03/15/4111c78f9d42ea98b546640b4d97c3c9.png" alt="4111c78f9d42ea98b546640b4d97c3c9.png" width="30" height="30" />
 )
 
 export default Logo
