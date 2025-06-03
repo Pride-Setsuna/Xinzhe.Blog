fe803f0129fd21b843188cf6963c36b1bd2320c4
Xinzhe.Gao <gao-yiqian@outlook.com>
2025-06-03 10:15:34 +0900

    修改了页面获取语言逻辑
    语言切换器

--- pages/page/[page].js
+++ pages/page/[page].js
@@ -1,49 +1,19 @@
-import Container from '@/components/Container'
-import BlogPost from '@/components/BlogPost'
-import Pagination from '@/components/Pagination'
-import { getAllPosts } from '@/lib/notion'
-import BLOG from '@/blog.config'
+import { getAllPosts, getAllTagsFromPosts } from '@/lib/notion'
+import SearchLayout from '@/layouts/search'
 
-const Page = ({ postsToShow, page, showNext }) => {
-  return (
-    <Container>
-      {postsToShow &&
-        postsToShow.map((post) => <BlogPost key={post.id} post={post} />)}
-      <Pagination page={page} showNext={showNext} />
-    </Container>
-  )
+export default function search({ tags, posts }) {
+  return <SearchLayout tags={tags} posts={posts} />
 }
-
-export async function getStaticProps(context) {
-  const { page } = context.params // Get Current Page No.
-  const posts = await getAllPosts({ onlyPost: true })
-  const postsToShow = posts.slice(
-    BLOG.postsPerPage * (page - 1),
-    BLOG.postsPerPage * page
-  )
-  const totalPosts = posts.length
-  const showNext = page * BLOG.postsPerPage < totalPosts
+export async function getStaticProps({ locale }) {
+  const posts = await getAllPosts({ onlyNewsletter: false, locale: locale || '' })
+  const shuffledPosts = posts.sort(() => Math.random() - 0.5)
+  const limitedPosts = shuffledPosts.slice(0, 10)
+  const tags = getAllTagsFromPosts(posts)
   return {
     props: {
-      page, // Current Page
-      postsToShow,
-      showNext
+      tags,
+      posts: limitedPosts
     },
     revalidate: 1
   }
 }
-
-export async function getStaticPaths() {
-  const posts = await getAllPosts({ onlyNewsletter: false })
-  const totalPosts = posts.length
-  const totalPages = Math.ceil(totalPosts / BLOG.postsPerPage)
-  return {
-    // remove first page, we 're not gonna handle that.
-    paths: Array.from({ length: totalPages - 1 }, (_, i) => ({
-      params: { page: '' + (i + 2) }
-    })),
-    fallback: true
-  }
-}
-
-export default Page
