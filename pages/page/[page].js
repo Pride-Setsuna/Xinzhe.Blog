b82c3e2353658d3a10ac2ad0d111a4092eb37a5f
Xinzhe.Gao <gao-yiqian@outlook.com>
2025-06-03 10:22:19 +0900

    Update
    Update

--- pages/page/[page].js
+++ pages/page/[page].js
@@ -1,19 +1,58 @@
-import { getAllPosts, getAllTagsFromPosts } from '@/lib/notion'
-import SearchLayout from '@/layouts/search'
+import Container from '@/components/Container'
+import BlogPost from '@/components/BlogPost'
+import Pagination from '@/components/Pagination'
+import { getAllPosts } from '@/lib/notion'
+import BLOG from '@/blog.config'
 
-export default function search({ tags, posts }) {
-  return <SearchLayout tags={tags} posts={posts} />
+const Page = ({ postsToShow, page, showNext }) => {
+  return (
+    <Container>
+      {postsToShow &&
+        postsToShow.map((post) => <BlogPost key={post.id} post={post} />)}
+      <Pagination page={page} showNext={showNext} />
+    </Container>
+  )
 }
-export async function getStaticProps({ locale }) {
-  const posts = await getAllPosts({ onlyNewsletter: false, locale: locale || '' })
-  const shuffledPosts = posts.sort(() => Math.random() - 0.5)
-  const limitedPosts = shuffledPosts.slice(0, 10)
-  const tags = getAllTagsFromPosts(posts)
+
+
+export async function getStaticProps(context) {
+  const { page } = context.params // 当前页码
+  const locale = context.locale || ''
+  const posts = await getAllPosts({ onlyPost: true, locale })
+  const postsToShow = posts.slice(
+    BLOG.postsPerPage * (page - 1),
+    BLOG.postsPerPage * page
+  )
+  const totalPosts = posts.length
+  const showNext = page * BLOG.postsPerPage < totalPosts
   return {
     props: {
-      tags,
-      posts: limitedPosts
+      page, // 当前页码
+      postsToShow,
+      showNext
     },
     revalidate: 1
   }
 }
+
+
+export async function getStaticPaths({ locales }) {
+  let paths = []
+  for (const locale of locales) {
+    const posts = await getAllPosts({ onlyNewsletter: false, locale })
+    const totalPosts = posts.length
+    const totalPages = Math.ceil(totalPosts / BLOG.postsPerPage)
+    paths = paths.concat(
+      Array.from({ length: totalPages - 1 }, (_, i) => ({
+        params: { page: '' + (i + 2) },
+        locale
+      }))
+    )
+  }
+  return {
+    paths,
+    fallback: true
+  }
+}
+
+export default Page
