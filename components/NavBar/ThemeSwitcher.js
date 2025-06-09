7827f800bd74af3c569b7cf52935b043433bc446
Xinzhe.Gao <gao-yiqian@outlook.com>
2025-06-09 23:10:55 +0900

    优化主题颜色显示
    自动跟随系统深浅色切换

--- components/NavBar/ThemeSwitcher.js
+++ components/NavBar/ThemeSwitcher.js
@@ -3,31 +3,29 @@ import { SunIcon, MoonIcon } from '@heroicons/react/outline'
 import { useTheme } from 'next-themes'
 
 const ThemeSwitcher = () => {
-  const { theme, setTheme } = useTheme()
+  const { theme, setTheme, systemTheme } = useTheme()
   const [hasMounted, setHasMounted] = useState(false)
 
   useEffect(() => {
     setHasMounted(true)
+    setTheme('system')
+    if (typeof window !== 'undefined') {
+      localStorage.removeItem('theme')
+    }
   }, [])
+
+  if (!hasMounted) return null
+
+  const isDark = (theme === 'system' ? systemTheme === 'dark' : theme === 'dark')
+
   return (
-    <>
-      <button
-        // title={`Toggle theme - current ${theme}`}
-        aria-label='ThemeSwitcher'
-        onClick={() =>
-          setTheme(
-            theme === 'light' ? 'dark' : theme === 'system' ? 'dark' : 'light'
-          )
-        }
-        className='p-2 ml-1 hover:bg-gray-200 dark:hover:bg-gray-700 cursor-pointer rounded-lg dark:text-gray-100'
-      >
-        {hasMounted && theme === 'dark' ? (
-          <MoonIcon className='h-5 w-5' />
-        ) : (
-          <SunIcon className='h-5 w-5' />
-        )}
-      </button>
-    </>
+    <button
+      aria-label='ThemeSwitcher'
+      onClick={() => setTheme(isDark ? 'light' : 'dark')}
+      className='p-2 ml-1 hover:bg-gray-200 dark:hover:bg-gray-700 cursor-pointer rounded-lg dark:text-gray-100'
+    >
+      {isDark ? <MoonIcon className='h-5 w-5' /> : <SunIcon className='h-5 w-5' />}
+    </button>
   )
 }
 
